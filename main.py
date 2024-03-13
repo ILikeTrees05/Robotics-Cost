6b33a8c6d341ee36630a134c4a323f499416ed13
Brandon Shalit <brandon.shalit@gmail.com>
2024-03-13 12:28:32 -0400

    Initial commit -- Working only for GOBILDA parts

---
+++ main.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup as bs
+from numpy import cumsum
+import requests
+import pandas as pd
+import streamlit as st
+
+
+def getGoBilda(link: str):
+    soup = bs(requests.get(link).content, "html.parser")
+    return (
+        float(
+            soup.find(attrs={"class": "price price--withoutTax"})
+            .get_text()
+            .split("/")[0]
+            .replace("$", "")
+        ),
+        soup.find(attrs={"class": "productView-title"}).get_text(),
+    )
+
+
+if __name__ == "__main__":
+    st.title("Robotics Parts Cost $$$")
+    inputCsv = st.file_uploader(
+        type=["csv"], accept_multiple_files=False, label="Upload CSV"
+    )
+    st.markdown(
+        "[Template Spreadsheet](https://docs.google.com/spreadsheets/d/1EE0nkI8h4506-zhxTSez_Uyiq6TKrHpjC8LqlnPCjn0/edit#gid=0)"
+    )
+    if inputCsv is not None:
+        partList = pd.read_csv(inputCsv)
+        partList = partList[pd.notnull(partList).any(axis=1)].reset_index(drop=True)
+        partList["ITEM QUANTITY"] = partList["ITEM QUANTITY"]
+        totalPrices = []
+        prices = []
+        itemNames = []
+        for i in partList.iterrows():
+            if i[1]["ITEM LINK"].__contains__("gobilda"):
+                indPrice, itemName = getGoBilda(i[1]["ITEM LINK"])
+                totalPrices.append(indPrice * i[1]["ITEM QUANTITY"])
+                prices.append(indPrice)
+                itemNames.append(itemName)
+        partList.insert(2, "TOTAL COST", cumsum(totalPrices))
+        partList.insert(2, "TOTAL ITEM PRICE", totalPrices)
+        partList.insert(2, "INDIVIDUAL ITEM PRICE", prices)
+        partList.insert(1, "ITEM NAME", itemNames)
+        st.dataframe(partList)
+        st.download_button("Download CSV", partList.to_csv())
