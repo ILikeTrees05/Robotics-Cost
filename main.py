52094a7251bcfc7cbd42b9b758a6099d112b9857
Brandon Shalit <brandon.shalit@gmail.com>
2024-03-14 08:23:28 -0400

    added functionality for Rev robotics, andymark, mcmastercarr, and WCP

--- main.py
+++ main.py
@@ -16,7 +16,50 @@ def getGoBilda(link: str):
         ),
         soup.find(attrs={"class": "productView-title"}).get_text(),
     )
-
+def getRevRobotics(link: str):
+    soup = bs(requests.get(link).content, "html.parser")
+    return (
+        float(
+            soup.find(attrs={"class": "price price--withoutTax price-primary"})
+            .get_text()
+            .split("/")[0]
+            .replace("$", "")
+        ),
+        soup.find(attrs={"class": "productView-title"}).get_text(),
+    )
+def getAndymark(link: str):
+    soup = bs(requests.get(link).content, "html.parser")
+    return (
+        float(
+            soup.find(attrs={"class": "product-prices__price"})
+            .get_text()
+            .split("/")[0]
+            .replace("$", "")
+        ),
+        soup.find(attrs={"class": "product-details__heading"}).get_text(),
+    )
+def getMcMasterCarr(link: str):
+    soup = bs(requests.get(link).content, "html.parser")
+    return (
+        float(
+            soup.find(attrs={"class": "PrceTxt"})
+            .get_text()
+            .split(" ")[0]
+            .replace("$", "")
+        ),
+        soup.find(attrs={"class": "Headers_productDetailHeaderPrimary__1Eaw5"}).get_text(),
+    )
+def getWCP(link: str):
+    soup = bs(requests.get(link).content, "html.parser")
+    return (
+        float(
+            soup.find(attrs={"class": "new-price"})
+            .get_text()
+            .split(" ")[0]
+            .replace("$", "")
+        ),
+        soup.find(attrs={"class": "tt-title"}).get_text(),
+    )
 
 if __name__ == "__main__":
     st.title("Robotics Parts Cost $$$")
@@ -36,6 +79,21 @@ if __name__ == "__main__":
         for i in partList.iterrows():
             if i[1]["ITEM LINK"].__contains__("gobilda"):
                 indPrice, itemName = getGoBilda(i[1]["ITEM LINK"])
+                totalPrices.append(indPrice * i[1]["ITEM QUANTITY"])
+                prices.append(indPrice)
+                itemNames.append(itemName)
+            elif i[1]["ITEM LINK"].__contains__("revrobotics"):
+                indPrice, itemName = getRevRobotics(i[1]["ITEM LINK"])
+                totalPrices.append(indPrice * i[1]["ITEM QUANTITY"])
+                prices.append(indPrice)
+                itemNames.append(itemName)
+            elif i[1]["ITEM LINK"].__contains__("andymark"):
+                indPrice, itemName = getAndymark(i[1]["ITEM LINK"])
+                totalPrices.append(indPrice * i[1]["ITEM QUANTITY"])
+                prices.append(indPrice)
+                itemNames.append(itemName)
+            elif i[1]["ITEM LINK"].__contains__("mcmaster"):
+                indPrice, itemName = getWCP(i[1]["ITEM LINK"])
                 totalPrices.append(indPrice * i[1]["ITEM QUANTITY"])
                 prices.append(indPrice)
                 itemNames.append(itemName)
